c5f3d2b293dc85cbb4fd99154f881308e02c4956
atheo810 <atheos810@gmail.com>
2023-11-03 23:24:19 +0800

    feat: link page and about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
   return (
     <>
       <Link href="/dashboard">Dashboard</Link>
-      <Link href="/error">Error</Link>
+      <hr />
+      <Link href="/about">About</Link>
     </>
   );
 }
