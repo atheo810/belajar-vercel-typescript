210a13c7cba32ece669e53911b123d65bd80d841
atheo810 <atheos810@gmail.com>
2023-11-03 23:13:44 +0800

    feat: dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,11 @@
-export default function page() {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard Page From Atheo",
+};
+
+export default function DashboardPage() {
   return (
     <>
       <h1>testing dari dashboard</h1>
