b7e9a97c61c6d27291192118e992fd30305fb415
atheo810 <atheos810@gmail.com>
2023-11-03 23:15:14 +0800

    feat: about Page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page",
+  description: "About Page from Atheo",
+};
+
+export default function PageAbout() {
+  return <div>disini adalah about</div>;
+}
