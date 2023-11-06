a8a840f595ea611d258e7c79ac3dbbe4e725b4c6
atheo810 <atheos810@gmail.com>
2023-11-06 19:19:24 +0800

    feat: navbar

---
+++ src/app/navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import "tailwindcss";
+
+export default function Navbar() {
+  return (
+    <nav className="flex flex-row">
+      <ul>
+        <li className="basis-1/2">
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+        <li className="basis-1/2">
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
