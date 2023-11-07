28f1c0cd9727f37ef7d7a591d8bc768ddd2fe8fe
atheo810 <atheos810@gmail.com>
2023-11-07 13:10:51 +0800

    feat: slashing on progress

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
-import "tailwindcss";
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row">
-      <ul className="no-underline">
-        <li className="basis-1/2">
-          <Link href="/dashboard">Dashboard</Link>
-        </li>
-        <li className="basis-1/2">
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
+    <nav className="relative">
+      <span className="flex">
+        <h1 className="text-rose-700 font-work text-lg font-bold leading-normal z-10 pl-24 pt-[46px] pr-[398px]">
+          Atheo Dev
+        </h1>
+
+        <ul className="flex flex-row pt-[46px]">
+          <li className="basis-1/2 items-center">
+            <Link className="no-underline" href="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+          <li className="basis-1/2 items-center">
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </span>
     </nav>
   );
 }
