c86e14198bd3d9a712b085107ce476cabac4b9a9
atheo810 <atheos810@gmail.com>
2023-11-07 13:49:44 +0800

    styling: navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,20 +2,29 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="relative">
-      <span className="flex">
-        <h1 className="text-rose-700 font-work text-lg font-bold leading-normal z-10 pl-24 pt-[46px] pr-[398px]">
+    <nav className="relative pt-[46px]">
+      <span className="flex flex-row">
+        <h1 className="text-rose-700 font-work text-lg font-bold leading-normal z-10 pl-24 pr-[398px]">
           Atheo Dev
         </h1>
 
-        <ul className="flex flex-row pt-[46px]">
-          <li className="basis-1/2 items-center">
-            <Link className="no-underline" href="/dashboard">
+        <ul className="flex flex-row">
+          <li className="pr-[37px]">
+            <Link className="" href="/dashboard">
               Dashboard
             </Link>
           </li>
-          <li className="basis-1/2 items-center">
+          <li className="pr-[37px]">
             <Link href="/about">About</Link>
+          </li>
+          <li className="pr-[37px]">
+            <Link href="/about">Our Service</Link>
+          </li>
+          <li className="pr-[37px]">
+            <Link href="/about">Our Project</Link>
+          </li>
+          <li className="">
+            <Link href="/about">Contact Us</Link>
           </li>
         </ul>
       </span>
