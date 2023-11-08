e05f6dab9e19cc3a2681d57d4aa91f10bcf91327
atheo810 <atheos810@gmail.com>
2023-11-08 15:34:15 +0800

    style: Text dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,11 +12,14 @@ export default function DashboardPage() {
       <div className="banner-img absolute min-h-[700px] bg-banner-image bg-cover w-[100%] bg-fixed bg-no-repeat brightness-50 h-[100%]"></div>
       <span className="banner-text z-20">
         <h1>
-          <span className="text-hijau font-sans text-[59px] text-white">
-            Disini Kita menulis judul
+          <span className="text-hijau font-Inter text-[59px] text-white font-bold leading-normal">
+            Bali Quality Dev is your Solution
           </span>
         </h1>
-        <p>text</p>
+        <p className="text-white font-work text-[27px] font-normal text-center">
+          We have a special team that handles your project. consisting of Team
+          Development, Team Designer, and Team Content Writer.
+        </p>
       </span>
     </div>
   );
